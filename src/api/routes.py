07c8dd99ef601bca74d399dcc585589c3e4911fc
MsterX2 <167225733+MsterX2@users.noreply.github.com>
2025-12-12 18:05:39 +0000

    Refactor user model and update command handling for test users

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, Users
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -19,4 +19,4 @@ def handle_hello():
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
 
-    return jsonify(response_body), 200
+    return 200
